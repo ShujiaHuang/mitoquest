ce9452234eb08d9a496979e9eb8847118290f778
Shujia Huang <huangshujia9@gmail.com>
2025-02-24 17:34:09 +0800

    fix the cmake problem

--- bin/make.sh
+++ bin/make.sh
@@ -1,2 +1,2 @@
-#g++ -o count_align_fragments count_align_fragments.cpp -lhts -lz -lcurl -pthread -O3
 g++ -O3 -fPIC ../scripts/count_align_fragments.cpp ../htslib/libhts.a -I ../htslib -lz -lm -lpthread -lcurl -llzma -lbz2 -o count_align_fragments
+g++ -O3 -fPIC ../src/*.cpp ../src/io/*.cpp ../htslib/libhts.a -I ../htslib -lz -lbz2 -lm -llzma -lpthread -lcurl -o mtvariantcaller
