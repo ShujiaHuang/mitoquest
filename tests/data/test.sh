1008175e856625ffacf3f16ed99b6d6c958a7f40
Shujia Huang <huangshujia9@gmail.com>
2025-03-06 23:40:28 +0800

    update

--- tests/data/test.sh
+++ tests/data/test.sh
@@ -2,3 +2,7 @@
 ../../bin/hmtk -f ce.fa.gz -o tt range.bam -r CHROMOSOME_MtDNA
 ../../bin/hmtk -f ce.fa.gz -o tt -r CHROMOSOME_I:1000-2400 range.bam 
 ../../bin/hmtk -t 4 -f ce.fa.gz -o tt -r CHROMOSOME_I:1000-2400 range.bam range.bam
+
+../../bin/hmtk -t 4 -f Homo_sapiens.GRCh38.chrM_rCRS.decoy.fa -r chrM -o tt 21200715BFF2A.cram 15100105TLL4A.cram 00115121204M17BFF2.cram
+
+
