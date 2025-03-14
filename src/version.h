eaccec32b35cbcd89e805f4357836f37967c52ee
Shujia Huang <huangshujia9@gmail.com>
2025-03-14 11:56:04 +0800

    1.4.0 -> 1.4.1

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 
 #define MITOQUEST_VERSION_MAJOR "1"
 #define MITOQUEST_VERSION_MINOR "4"
-#define MITOQUEST_VERSION_PATCH "0"
-#define MITOQUEST_VERSION "1.4.0"
+#define MITOQUEST_VERSION_PATCH "1"
+#define MITOQUEST_VERSION "1.4.1"
 #define MITOQUEST_AUTHOR "Shujia Huang"
 #define MITOQUEST_AUTHOR_EMAIL "hshujia@qq.com"
 #define MITOQUEST_DESCRIPTION "mitoquest: Human Mitochondrial sequencing data Analysis Toolkit"
