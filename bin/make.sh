f5cde8dddeae265492cd39f9e24c35aa896ad37f
Shujia Huang <huangshujia9@gmail.com>
2025-03-22 09:26:37 +0800

    small modify

--- bin/make.sh
+++ bin/make.sh
@@ -1,4 +1,4 @@
-rsync -avtP -e "ssh -i ~/.ssh/gzfezx_shhlixy_3.id -p 6666" gzfezx_shhlixy_3@172.16.31.31:~/BioSoftware/mitoquest/bin/mitoquest ./mitoquest-linux-static
+rsync -avtP -e "ssh -i ~/.ssh/gzfezx_shhlixy_3.id -p 6666" gzfezx_shhlixy_3@172.16.31.31:~/BioSoftware/local/bin/mitoquest ./mitoquest-linux-static
 
 #g++ -O3 -fPIC ../scripts/count_align_fragments.cpp ../htslib/libhts.a -I ../htslib -lz -lm -lpthread -lcurl -llzma -lbz2 -lssl -lcrypto -o count_align_fragments
 
