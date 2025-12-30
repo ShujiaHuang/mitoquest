4358e4037d23b06e2ef20198fad0d671d31da6f0
Shujia Huang <huangshujia9@gmail.com>
2025-12-30 22:56:07 +0800

    update

--- bin/make.sh
+++ bin/make.sh
@@ -2,7 +2,7 @@ rsync -avtP -e "ssh -i ~/.ssh/gzfezx_shhlixy_3.id -p 6666" gzfezx_shhlixy_3@172.
 
 #g++ -O3 -fPIC ../scripts/count_align_fragments.cpp ../htslib/libhts.a -I ../htslib -lz -lm -lpthread -lcurl -llzma -lbz2 -lssl -lcrypto -o count_align_fragments
 
-g++ -o count_align_fragments ../scripts/count_align_fragments.cpp -lhts -lz -lcurl -pthread -O3 -fPIC
 g++ -o mitoquest -O3 -fPIC ../src/*.cpp ../src/io/*.cpp -I ../src -lhts -lz -lbz2 -lm -llzma -lpthread -lcurl -lssl -lcrypto
+g++ -o mtcopynum -O3 -fPIC ../scripts/mt_copynum.cpp ../src/algorithm.cpp ../src/mt_utils.cpp ../src/io/iobgzf.cpp ../src/io/fasta.cpp ../src/io/utils.cpp -I ../src -lhts -lz -lbz2 -lm -llzma -lpthread -lcurl -lssl -lcrypto
 
 
