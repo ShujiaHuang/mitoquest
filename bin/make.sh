5d112e2bcb8a7e239e609619b8be3a90f7e1c315
Shujia Huang <huangshujia9@gmail.com>
2026-05-23 17:31:10 +0800

    i donot need the manual make.sh

--- bin/make.sh
+++
@@ -1,8 +0,0 @@
-rsync -avtP -e "ssh -i ~/.ssh/gzfezx_shhlixy_3.id -p 6666" gzfezx_shhlixy_3@172.16.31.31:~/BioSoftware/local/bin/mitoquest ./mitoquest-linux-static
-
-#g++ -O3 -fPIC ../scripts/count_align_fragments.cpp ../htslib/libhts.a -I ../htslib -lz -lm -lpthread -lcurl -llzma -lbz2 -lssl -lcrypto -o count_align_fragments
-
-g++ -o mitoquest -O3 -fPIC ../src/*.cpp ../src/io/*.cpp -I ../src -lhts -lz -lbz2 -lm -llzma -lpthread -lcurl -lssl -lcrypto
-# NOTE: `mtcopynum` is now folded into `mitoquest` as the `mitoquest copynum` subcommand.
-
-
